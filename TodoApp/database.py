92ed9e927394bc70cf07a726a0947a6fe5f80e4d
thomasfere <thomasferentinos@gmail.com>
2026-08-24 22:02:52 +0300

    Read DATABASE_URL from env

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,15 +1,17 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQL_ALCHEMY_DATABASE_URL = 'postgresql://postgres:test1234!@localhost/TodoApplicationDatabase'
+SQL_ALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:test1234!@localhost/TodoApplicationDatabase"  # local fallback
+)
+
+if SQL_ALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQL_ALCHEMY_DATABASE_URL = SQL_ALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 
 engine = create_engine(SQL_ALCHEMY_DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
-
-
-
-
