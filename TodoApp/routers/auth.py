d8ec460cf61a8e887b07fcc8ae2b676e583785e1
thomasfere <thomasferentinos@gmail.com>
2026-08-24 22:20:06 +0300

    Fix case-sensitive path for templates directory

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -44,7 +44,7 @@ def get_db():
 
 db_dependency = Annotated[SessionLocal, Depends(get_db)]
 
-templates = Jinja2Templates(directory="TodoApp/Templates")
+templates = Jinja2Templates(directory="TodoApp/templates")
 
 ### Pages ###
 
