ad8de533862ab50d736d87fec5306cbc90a2b681
thomasfere <thomasferentinos@gmail.com>
2026-08-31 18:14:44 +0300

    Add .env variables

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,12 +1,12 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQL_ALCHEMY_DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:test1234!@localhost/TodoApplicationDatabase"  # local fallback
-)
+load_dotenv()
+
+SQL_ALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 if SQL_ALCHEMY_DATABASE_URL.startswith("postgres://"):
     SQL_ALCHEMY_DATABASE_URL = SQL_ALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
